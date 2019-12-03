04ff2601a58b04dab7287c36dc48f7ddb06f443b
Simpan1001 <simon.springer@edu.huddinge.se>
2019-12-03 12:47:55 +0100

    Added html to server reachability

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
+
+//DONT FORGET TO CHANGE THE PORT BEFORE YOU PUSH TO MASTER
+
 const express = require('express')
 const app = express()
 const port = process.env.PORT
 const localport = 3000
 
+app.use(express.static('Public'));
+
 app.get('/', function (req, res) {
     res.send('Hello World!');
-})
+});
+
+app.get('/html', function(req, res) {
+    res.sendfile(__dirname + '/Public/index.html');
+});
 
 var server = app.listen(port, function () {
     console.log("Server is up and running at " + port);
-})
+});
